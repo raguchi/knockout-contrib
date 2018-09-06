63a2ebf30e54e2abc31c8bc9821d53490200a925
Casey Webb <notcaseywebb@gmail.com>
2018-09-06 14:28:57 -0500

    chore(punches): Remove unnecessary test support files

---
+++ packages/punches/__tests__/preprocessBindingPropertySpec.js
@@ -0,0 +1,27 @@
+describe('Preprocess binding properties', function() {
+  beforeEach(jasmine.prepareTestNode)
+
+  it('Should preprocess the specified binding property', function() {
+    try {
+      var value
+      ko.bindingHandlers['a'] = {
+        init: function(element, valueAccessor) {
+          value = valueAccessor()
+        }
+      }
+      ko.punches.preprocessBindingProperty.addPreprocessor(
+        'a',
+        'b',
+        function() {
+          return '"new value"'
+        }
+      )
+      testNode.innerHTML =
+        '<div data-bind=\'a: {b: "old value", c: "unrelated value"}\'></div>'
+      ko.applyBindings(null, testNode)
+      expect(value).toEqual({ b: 'new value', c: 'unrelated value' })
+    } finally {
+      delete ko.bindingHandlers['a']
+    }
+  })
+})
